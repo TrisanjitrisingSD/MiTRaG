023734612ddd297a80a50a5c08f8d7b10c51de57
TrisanjitrisingSD <trisanjitdas7@gmail.com>
2026-07-10 22:21:10 +0530

    error message fixed

--- ask_question_to_LLM.py
+++ ask_question_to_LLM.py
@@ -44,6 +44,6 @@ def ask_llm(question):
         # answer = llama_answer(prompt) not possible during deployment 
         answer="Server is Busy With Other Query"
         return {
-            "answer":answer,
+            "answer":e,
             "model":"Gemini"
         }
